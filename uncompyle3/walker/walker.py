eb1a310592bfb43c73861f24f7c32ce80762e9e5
DarkPhoenix <phoenix@mail.ru>
2012-09-07 16:43:37 +0400

    Add support for assignments  to walker

--- uncompyle3/walker/walker.py
+++ uncompyle3/walker/walker.py
@@ -1,17 +1,17 @@
 import re
-import sys
 
-from uncompyle3.parser.astnode import ASTNode
 from uncompyle3.utils.spark import GenericASTTraversal
 from uncompyle3.utils.debug import debug
 
 
 TABLE_R = {
-     "CALL_FUNCTION": ('%c(%P)', 0, (1,-1,', ',100))
+     "CALL_FUNCTION": ("%c(%P)", 0, (1, -1, ", ", 100))
 }
 TABLE_DIRECT = {
-    'call_stmt':    ( '%|%p\n', (0,200)),
-    'LOAD_NAME':    ( '%{pattr}', ),
+    "call_stmt":    ("%|%p\n", (0, 200)),
+    "LOAD_NAME":    ("%{pattr}",),
+    "assign":       ("%|%c = %p\n", -1, (0, 200)),
+    "STORE_NAME":   ("%{pattr}",),
 }
 
 
@@ -19,22 +19,22 @@ MAP_DIRECT = (TABLE_DIRECT, )
 MAP_R = (TABLE_R, -1)
 
 MAP = {
-    'stmt':        MAP_R,
-    'call_function':        MAP_R,
+    "stmt":        MAP_R,
+    "call_function":        MAP_R,
 }
 
 PRECEDENCE = {
-    'call_function':        2,
+    "call_function":        2,
 }
 
 # " g  %  [  - 5  ]   f  "
 # " g  %  [  - 5  ]    {  f  } "s
-escape = re.compile(r'''
+escape = re.compile(r"""
             (?P<prefix> [^%]* )
             % ( \[ (?P<child> -? \d+ ) \] )?
                 ((?P<type> [^{] ) |
                  ( [{] (?P<expr> [^}]* ) [}] ))
-        ''', re.VERBOSE)
+        """, re.VERBOSE)
 
 
 
@@ -85,33 +85,33 @@ class Walker(GenericASTTraversal):
         m = escape.search(fmt)
         while m:
             i = m.end()
-            debug("writing prefix: '{}'".format(m.group('prefix')))
-            self.write(m.group('prefix'))
+            debug("writing prefix: \"{}\"".format(m.group("prefix")))
+            self.write(m.group("prefix"))
 
-            typ = m.group('type') or '{'
+            typ = m.group("type") or "{"
             node = startnode
             try:
-                if m.group('child'):
+                if m.group("child"):
                     debug("using child")
-                    node = node[int(m.group('child'))]
+                    node = node[int(m.group("child"))]
             except:
                 print(node.__dict__)
                 raise
             debug("type:", typ)
-            if   typ == '%':    self.write('%')
-            elif typ == '+':    self.indentMore()
-            elif typ == '-':    self.indentLess()
-            elif typ == '|':    self.write(self.indent)
-            elif typ == 'p':
+            if   typ == "%":    self.write("%")
+            elif typ == "+":    self.indentMore()
+            elif typ == "-":    self.indentLess()
+            elif typ == "|":    self.write(self.indent)
+            elif typ == "p":
                 p = self.prec
                 (index, self.prec) = entry[arg]
                 self.preorder(node[index])
                 self.prec = p
                 arg += 1
-            elif typ == 'c':
+            elif typ == "c":
                 self.preorder(node[entry[arg]])
                 arg += 1
-            elif typ == 'P':
+            elif typ == "P":
                 p = self.prec
                 low, high, sep, self.prec = entry[arg]
                 remaining = len(node[low:high])
@@ -123,9 +123,9 @@ class Walker(GenericASTTraversal):
                         self.write(sep)
                 self.prec = p
                 arg += 1
-            elif typ == '{':
+            elif typ == "{":
                 d = node.__dict__
-                expr = m.group('expr')
+                expr = m.group("expr")
                 try:
                     self.write(eval(expr, d, d))
                 except:
@@ -142,19 +142,23 @@ class Walker(GenericASTTraversal):
     def n_expr(self, node):
         debug("walker.n_expr()")
         p = self.prec
-        if node[0].type.startswith('binary_expr'):
+        if node[0].type.startswith("binary_expr"):
             n = node[0][-1][0]
         else:
             n = node[0]
         self.prec = PRECEDENCE.get(n,-2)
         debug("picked node {} with precedence {}, old precedence {}".format(n.type, self.prec, p))
-        if n == 'LOAD_CONST' and repr(n.pattr)[0] == '-':
+        if n == "LOAD_CONST" and repr(n.pattr)[0] == "-":
             self.prec = 6
         if p < self.prec:
-            self.write('(')
+            self.write("(")
             self.preorder(node[0])
-            self.write(')')
+            self.write(")")
         else:
             self.preorder(node[0])
         self.prec = p
+        self.prune()
+
+    def n_LOAD_CONST(self, node):
+        self.write(node.pattr)
         self.prune()
