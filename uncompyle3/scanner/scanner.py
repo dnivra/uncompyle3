030dc8a3b22e42b594d0757593399eecd725f03f
DarkPhoenix <phoenix@mail.ru>
2012-09-14 18:38:11 +0400

    Avoid using starred import

--- uncompyle3/scanner/scanner.py
+++ uncompyle3/scanner/scanner.py
@@ -1,6 +1,6 @@
 import marshal
 
-from .dis import *
+from . import dis
 from .token import Token
 
 
@@ -15,7 +15,7 @@ class Scanner:
         """Convert code object into a sequence of tokens."""
         tokens = []
         code = co.co_code
-        linestarts = dict(findlinestarts(co))
+        linestarts = dict(dis.findlinestarts(co))
         n = len(code)
         i = 0
         extended_arg = 0
@@ -23,29 +23,29 @@ class Scanner:
         while i < n:
             op = code[i]
             current_token = token_cls()
-            current_token.type = opname[op]
+            current_token.type = dis.opname[op]
             current_token.offset = i
             current_token.linestart = True if i in linestarts else False
             i = i+1
-            if op >= HAVE_ARGUMENT:
+            if op >= dis.HAVE_ARGUMENT:
                 oparg = code[i] + code[i+1]*256 + extended_arg
                 extended_arg = 0
                 i = i+2
-                if op == EXTENDED_ARG:
+                if op == dis.EXTENDED_ARG:
                     extended_arg = oparg*65536
 
                 current_token.attr = oparg
-                if op in hasconst:
+                if op in dis.hasconst:
                     current_token.pattr = repr(co.co_consts[oparg])
-                elif op in hasname:
+                elif op in dis.hasname:
                     current_token.pattr = co.co_names[oparg]
-                elif op in hasjrel:
+                elif op in dis.hasjrel:
                     current_token.pattr = repr(i + oparg)
-                elif op in haslocal:
+                elif op in dis.haslocal:
                     current_token.pattr = co.co_varnames[oparg]
-                elif op in hascompare:
-                    current_token.pattr = cmp_op[oparg]
-                elif op in hasfree:
+                elif op in dis.hascompare:
+                    current_token.pattr = dis.cmp_op[oparg]
+                elif op in dis.hasfree:
                     if free is None:
                         free = co.co_cellvars + co.co_freevars
                     current_token.pattr = free[oparg]
