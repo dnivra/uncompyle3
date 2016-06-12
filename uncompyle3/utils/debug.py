1fe4b0dd2e33c940a2010d68b4c6865be23c16ee
DarkPhoenix <phoenix@mail.ru>
2016-06-12 18:20:56 +0300

    Update paths
    Still hardcoded, but who cares, eh?

--- uncompyle3/utils/debug.py
+++ uncompyle3/utils/debug.py
@@ -1,9 +1,9 @@
-f = open("/home/dfx/src/uncompyle3/debug.log", "w")
+f = open('/home/dfx/src/decompilers/uncompyle3/uncompyle3/debug.log', 'w')
 f.close()
 
 def debug(*data):
-    line = " ".join(str(i) for i in data) + "\n"
-    f = open("/home/dfx/src/uncompyle3/debug.log", "a")
+    line = ' '.join(str(i) for i in data) + '\n'
+    f = open('/home/dfx/src/decompilers/uncompyle3/uncompyle3/debug.log', 'a')
     f.write(line)
     f.close()
     #print(line)
