0db21d0d234004a531aa4ee0348deeb3225d8bcb
DarkPhoenix <phoenix@mail.ru>
2016-06-12 20:31:47 +0300

    Get rid of hardcoded log paths

--- uncompyle3/utils/debug.py
+++ uncompyle3/utils/debug.py
@@ -1,9 +1,20 @@
-f = open('/home/dfx/src/decompilers/uncompyle3/uncompyle3/debug.log', 'w')
+import os
+import sys
+
+
+def get_log_path():
+    path = os.path.dirname(os.path.realpath(os.path.abspath(sys.argv[0])))
+    path = os.path.join(path, 'debug.log')
+    return path
+
+f = open(get_log_path(), 'w')
 f.close()
+
+
 
 def debug(*data):
     line = ' '.join(str(i) for i in data) + '\n'
-    f = open('/home/dfx/src/decompilers/uncompyle3/uncompyle3/debug.log', 'a')
+    f = open(get_log_path(), 'a')
     f.write(line)
     f.close()
     #print(line)
